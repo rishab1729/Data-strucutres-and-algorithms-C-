91f799e57e02d58ee2bd8be5b1c2f102a98c1eef
Rishab Sharma <90676987+rishab1729@users.noreply.github.com>
2022-02-15 00:33:08 +0530

    Create Bubble_Sort.cpp

---
+++ Bubble_Sort.cpp
@@ -0,0 +1,14 @@
+/*
+BUBBLE SORT:
+Bubble sort is a simple sorting algoritm that repeatedly steps through the arraym compares adjaced elements and swaps them if they are in the wrong order. The pass through 
+the array is repeated unitl the array is sorted.
+
+Algoritm:
+
+for (index  = 0 to input.length-1) {
+  for (j = 0 to j < input.lenght-1-index) {
+    if (input[j] > input[j+1])
+      swap input[J+1] & input[j]
+  }
+ }  
+  */
