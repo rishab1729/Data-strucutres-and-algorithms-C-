d5785c4cec5a1b996c374d97adb486405bdf5657
Rishab Sharma <90676987+rishab1729@users.noreply.github.com>
2022-03-01 11:32:45 +0530

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -28,3 +28,46 @@ for (i =0; to i = input.length -1
 }
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertion_sort(int input[], int n){
+    for (int i =0; i< n; i++)
+    {
+        int temp = input[i];
+        bool hasMoved = false;
+        bool notPlaced = true;
+        for (int j = i - 1;j>=0;j--){
+            if (input[j] > temp){
+                input[j+1] = input[j];
+                hasMoved = true;
+            }
+            else
+            {
+                input[j+1] =  temp;
+                notPlaced = false;
+                break;
+            }
+        }
+    if (hasMoved && notPlaced)
+        input[0] = temp;
+    }
+
+}
+
+int main()
+{
+	int arr[] = {89,56,78,11,23,5,32};
+	int n = *(&arr + 1) - arr,i=0;
+
+	insertion_sort(arr, n);
+	
+	while (i<n)
+	{
+	    cout << arr[i++] << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
+
