1f049543ac4464a78e710646ab9b1149df57fb21
Rishab Sharma <90676987+rishab1729@users.noreply.github.com>
2022-03-02 14:10:06 +0530

    Update selection_sort.cpp

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -19,3 +19,35 @@ for ( i=input.length-1 to i>0 ){
   }
 */
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n)
+{
+	int i, j, min_idx;
+
+	for (i = 0; i < n-1; i++)
+	{
+		min_idx = i;
+		for (j = i+1; j < n; j++)
+		if (arr[j] < arr[min_idx])
+			min_idx = j;
+
+		swap(arr[min_idx], arr[i]);
+	}
+}
+
+int main()
+{
+	int arr[] = {88,4,55,21,64,24};
+	int n = *(&arr+1)-arr,i=0;
+	
+	selection_sort(arr, n);
+	
+	cout << "Sorted array: \n";
+	while (i<n)
+		cout << arr[i++] << " ";
+	cout << endl;
+	
+	return 0;
+}
