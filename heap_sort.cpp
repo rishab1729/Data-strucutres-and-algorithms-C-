91d2da201b9fbdb49d5b0a6579a2a776ea0e0996
Rishab Sharma <90676987+rishab1729@users.noreply.github.com>
2022-03-03 10:12:40 +0530

    Create heap_sort.cpp

---
+++ heap_sort.cpp
@@ -0,0 +1,17 @@
+/*
+Heapsort can be thought of an improved selection sort: like selection sort, heapsort divides its input into a sorted and an unsorted region, and it itertively shrinks the unsorted
+region by extracting the largest element from it  and inserting it into the sorted region. However, heapsort does not waste time with a linear-time scan; rather, heap sort maintains 
+the unsorted region in a heap data strucutre to more quickly find the largest/smallest element in each step.
+
+sort(){
+int size = input.length
+ for (int i = size/2-1 to 0)
+ heapify(size, i)
+ 
+ for  (int i = size-1 to 0) {
+ swap input[i] & input[0]
+ heapify(i,0)
+ }
+}
+
+*/
